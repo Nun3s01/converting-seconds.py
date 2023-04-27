dabe3b84652061754b696f1584ca98ff8c933570
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2023-04-27 16:39:31 -0300

    Update Data Input - Seconds Conversion.py
    patch 0.1

--- Data Input - Seconds Conversion.py
+++ Data Input - Seconds Conversion.py
@@ -1,11 +1,15 @@
 ("Algorithm for converting seconds");   
 seconds = int(input("Please, tell me how many seconds you want to convert? "))
 
-days = (seconds) // 86400
+years = (seconds) // 31104000
+secRestant = (seconds) % 31104000
+months = secRestant // 2592000
+secRestant = (seconds) % 2592000
+days = secRestant // 86400
 secRestant = (seconds) % 86400
 hours = secRestant // 3600
 secRestant = (seconds) % 3600
 minutes = secRestant // 60
 secRestant = (seconds) % 60
-seconds_end_result = (seconds) % 60
-print(seconds, "seconds, is:", days, "days,", hours, "hours,", minutes, "minutes,", seconds_end_result, "seconds.")
+seconds_end_result = secRestant % 60
+print(f"{seconds} seconds, is: {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes, {seconds_end_result} seconds.")
